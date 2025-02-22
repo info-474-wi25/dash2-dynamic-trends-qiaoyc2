1de838857807f22163df603ad9b9baf545b3038a
Katharina Cheng <qiaoyc2@uw.edu>
2025-02-21 20:55:25 -0800

    group data by month and recalculate average temp

--- main.js
+++ main.js
@@ -4,14 +4,7 @@ const width = 900 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
 // Create SVG containers for both charts
-const svg1_RENAME = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
-const svg2_RENAME = d3.select("#lineChart2")
+const svgLine = d3.select("#lineChart1") // If you change this ID, you must change it in index.html too
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -22,8 +15,31 @@ const svg2_RENAME = d3.select("#lineChart2")
 // const tooltip = ...
 
 // 2.a: LOAD...
-d3.csv("YOUR_CSV_NAME.csv").then(data => {
+d3.csv("weather.csv").then(data => {
     // 2.b: ... AND TRANSFORM DATA
+    // Convert Date.Full to a Date object and extract Year-Month
+    const parseDate = d3.timeParse("%Y-%m-%d"); // Adjust based on your date format
+    const formatMonth = d3.timeFormat("%Y-%m");
+
+    data.forEach(d => {
+        d.date = parseDate(d["Date.Full"]);
+        d.yearMonth = formatMonth(d.date); // Extract YYYY-MM
+        d.avgTemp = +d["Data.Temperature.Avg Temp"]; // Convert temp to number
+    });
+
+    // Group data by Station.Location and Year-Month
+    const nestedData = d3.groups(data, d => d["Station.Location"], d => d.yearMonth);
+
+    // Compute monthly average temperature
+    const processedData = nestedData.map(([location, months]) => {
+        return months.map(([month, records]) => ({
+            location: location,
+            yearMonth: d3.timeParse("%Y-%m")(month), // Convert to Date format
+            avgTemp: d3.mean(records, d => d.avgTemp)
+        }));
+    }).flat();
+
+    console.log("Processed Data:", processedData);
 
     // 3.a: SET SCALES FOR CHART 1
 
