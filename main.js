ca01c06369026b3bd68dabfd26b3fd8a02449434
Katharina Cheng <qiaoyc2@uw.edu>
2025-02-28 21:14:56 -0800

    done the visualization

--- main.js
+++ main.js
@@ -39,40 +39,75 @@ d3.csv("weather.csv").then(data => {
         }));
     }).flat();
 
-    console.log("Processed Data:", processedData);
+    // const dataByLocation = d3.groups(processedData, d => d.location);
+    const filteredData = processedData.filter(d => d.location === 'Birmingham, AL')
 
+    // const categories = d3.rollup(categorizedData,
+    //     v => d3.rollup(v,
+    //         values => values.length,
+    //         d => d.year
+    //     ),
+    //     d => d.categoryGroup
+    // );
+    console.log(nestedData)
     // 3.a: SET SCALES FOR CHART 1
+    const [minDate, maxDate] = d3.extent(processedData, d => d.yearMonth);
 
+    // 2. Offset the maximum date by 1 month
+    const maxDatePlusOneMonth = d3.timeMonth.offset(maxDate, 1);
+    const xScale = d3.scaleTime()
+        .domain([minDate, maxDatePlusOneMonth])
+        .range([0, width]);
 
+    const yScale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0])
+        .nice();
+
+    const locations = Array.from(nestedData.keys());
+    const colorScale = d3.scaleOrdinal()
+        .domain(locations)
+        .range(d3.schemeCategory10);
+
+    const line = d3.line()
+        .x(d => xScale(d.yearMonth))
+        .y(d => yScale(d.avgTemp))
+
+    const dataArray = Array.from(nestedData.entries());
+    console.log(processedData)
     // 4.a: PLOT DATA FOR CHART 1
-
+    svgLine.selectAll(".line")
+        .data([filteredData])
+        .enter()
+        .append("path")
+        .attr("class","line")
+        .attr("d", line) // need to use the d funciton to plot multiple lines
+        .attr("stroke", "steelblue") // don't forget update the color when we need multiple lines
+        .attr("fill", "none");
 
     // 5.a: ADD AXES FOR CHART 1
+    svgLine.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y-%m")))
+    .selectAll("text")
+    .attr("transform", "rotate(-45)")
+    .style("text-anchor", "end");
 
+    svgLine.append("g")
+    .call(d3.axisLeft(yScale));
+    
 
     // 6.a: ADD LABELS FOR CHART 1
+    svgLine.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -margin.left + 15)
+    .attr("text-anchor", "middle")
+    .text("Average Temperature (°C)");
 
 
     // 7.a: ADD INTERACTIVITY FOR CHART 1
     
 
-    // ==========================================
-    //         CHART 2 (if applicable)
-    // ==========================================
-
-    // 3.b: SET SCALES FOR CHART 2
-
-
-    // 4.b: PLOT DATA FOR CHART 2
-
-
-    // 5.b: ADD AXES FOR CHART 
-
-
-    // 6.b: ADD LABELS FOR CHART 2
-
-
-    // 7.b: ADD INTERACTIVITY FOR CHART 2
-
-
+    
 });
